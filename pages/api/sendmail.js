9f19ad4f600cd41c3407f1a2a633be7bce1adb6f
Johannes Langosch <johannes.langosch@googlemail.com>
2022-09-14 20:31:24 +0200

    01 first commit

---
+++ pages/api/sendmail.js
@@ -0,0 +1,62 @@
+import nodemailer from "nodemailer";
+import sendEmail from "lib/email.js";
+
+// export default async function handler(req, res) {
+//   const transporter = nodemailer.createTransport(process.env.EMAIL_SERVER);
+
+//   transporter.sendMail(
+//     {
+//       from: process.env.EMAIL_FROM,
+//       to: to,
+//       subject: subject,
+//       html: body,
+//     },
+//     function (err, info) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         ok;
+//         sendEmail(
+//           "johannes.langosch@nexgo.de",
+//           "New booking",
+//           `${req.body.firstname} ${req.body.surname} `
+//         );
+//       }
+//     }
+//   );
+
+//   res.end();
+// }
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(501).end();
+  }
+
+  if (req.method === "POST") {
+    const transporter = nodemailer.createTransport({
+      port: process.env.EMAIL_SERVER_PORT,
+      host: process.env.EMAIL_SERVER_HOST,
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+      },
+      secure: true,
+    });
+    const mailData = {
+      from: process.env.EMAIL_FROM,
+      sender: "Gruppenpostfach - do not answer",
+      to: "johannes.langosch@gmail.com",
+      subject: `Beantragung Berechtigung für ${req.body.surname} ${req.body.pnr}`,
+      text: req.body.description,
+      html: `<div>${req.body.description}</div><p>Sent from:
+    ${req.body.email}</p>`,
+    };
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) console.log(err);
+      else console.log(info);
+    });
+    res.status(200);
+    res.end();
+  }
+}
